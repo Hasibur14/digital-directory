7f917138c9cc44b79b89c56d27f03acde4a8bbe4
hasibur14 <hasib141415@gmail.com>
2024-09-02 01:17:06 +0600

    [ADD] navbar and table

---
+++ client/src/page/Home/Home.jsx
@@ -0,0 +1,57 @@
+
+const Home = () => {
+    return (
+        <div className="container mx-auto my-10">
+            <div>
+                <div className="flex justify-between shadow-2xl border rounded-xl w-full p-6">
+                    <h2 className="text-xl font-bold">Last Edited</h2>
+
+                    <div className="bg-cyan-200 flex space-x-3 rounded-full p-3">
+                        <div className="w-8 p-1 text-center font-bold rounded-box bg-white">
+                            A
+                        </div>
+                        <div className="w-8 p-1 text-center font-bold rounded-box bg-white">
+                            B
+                        </div>
+                        <div className="w-8 p-1 text-center font-bold rounded-box bg-white">
+                            C
+                        </div>
+                        <div className="w-8 p-1 text-center font-bold rounded-box bg-white">
+                            D
+                        </div>
+                    </div>
+                    <div className="bg-cyan-200 flex space-x-3 rounded-full p-3">
+                        <div className="w-8 p-1 text-center font-bold rounded-box bg-white">
+                            E
+                        </div>
+                        <div className="w-8 p-1 text-center font-bold rounded-box bg-white">
+                            F
+                        </div>
+                        <div className="w-8 p-1 text-center font-bold rounded-box bg-white">
+                            G
+                        </div>
+                        <div className="w-8 p-1 text-center font-bold rounded-box bg-white">
+                            G
+                        </div>
+                    </div>
+                    <div className="bg-cyan-200 flex space-x-3 rounded-full p-3">
+                        <div className="w-8 p-1 text-center font-bold rounded-box bg-white">
+                            I
+                        </div>
+                        <div className="w-8 p-1 text-center font-bold rounded-box bg-white">
+                            B
+                        </div>
+                        <div className="w-8 p-1 text-center font-bold rounded-box bg-white">
+                            J
+                        </div>
+                        <div className="w-8 p-1 text-center font-bold rounded-box bg-white">
+                            K
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
